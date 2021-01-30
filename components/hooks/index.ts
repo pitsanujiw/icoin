23843dd94f0780cfa49270ea1613d230edbba2b8
Andy D. Ng <itc.anhduy@gmail.com>
2021-01-30 15:28:11 +0700

    [ANDY] Add LivePrices

---
+++ components/hooks/index.ts
@@ -0,0 +1,3 @@
+import useLivePrices from './useLivePrices'
+
+export { useLivePrices }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>ICoin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
