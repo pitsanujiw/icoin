1876103d532d5077852566832d8f6f653b2a6725
Andy D. Ng <itc.anhduy@gmail.com>
2021-01-31 14:40:44 +0700

    [ANDY] Fix material-ui with nextjs (#5)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { ThemeProvider, CssBaseline } from '@material-ui/core'
 import { TopBar, Header, LivePrices } from 'components'
-import { ThemeProvider } from '@material-ui/core'
+import { useEffect } from 'react'
 import Theme from 'styles'
 import Store from 'states'
 import 'styles/globals.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
     <Provider store={Store}>
       <ThemeProvider theme={Theme}>
+        <CssBaseline />
         <LivePrices />
         <TopBar />
         <Header />
